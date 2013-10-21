17a882ea9f94af31195d8c50cf9bb95b8d2650d6
edooby <rossi.he@gmail.com>
2013-10-21 15:52:31 +0200

    add dedicated function to add user in tools.php creation of inscription form

--- www/inc/tools.php
+++ www/inc/tools.php
@@ -5,6 +5,86 @@
 
 $db = new PDO('mysql:host=localhost;dbname=webannonces','webannonces','webannonces');
 
+//DB connection + requests
+function connect_webannonces_db()
+	{
+		$connect = new PDO('mysql:host=localhost;dbname=webannonces','webannonces','webannonces');
+		return $connect;
+	}
+
+function is_username_already_exists($db, $username)
+	{
+		$query_prepare = 'SELECT * FROM `users` WHERE username= :username';
+		$query= $db->prepare($query_prepare);
+		$query->bindParam(':username', $username, PDO::PARAM_STR);
+		$query->execute();
+		$count = $query->rowCount();
+		if ($count == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+function is_email_already_exists($db, $email)
+	{
+		$query_prepare = 'SELECT * FROM `users` WHERE email= :email';
+		$query= $db->prepare($query_prepare);
+		$query->bindParam(':email', $email, PDO::PARAM_STR);
+		$query->execute();
+		$count = $query->rowCount();
+		if ($count == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	
+//form
+function error_by_field($errors, $field)
+	{
+		$result = array();
+		foreach($errors as $error)
+		{
+			if ($error['field']=== $field)
+			{
+				$result[]=$error['message'];
+			}
+		}
+		return $result;
+	}
+	
+function save_origin_value_from_form($form, $field)
+	{
+		$result = '';
+		if(isset($form[$field]))
+		{
+			$result = stripslashes($form[$field]);
+		}
+		return $result;
+	}
+
+function add_newuser($db, $username, $pwd, $email)
+	{
+		$query_prepare = 'INSERT INTO users (username, password, email, date_inscription)
+			VALUES (:username, :password, :email, :date_inscription)';
+		$requete= $db->prepare($query_prepare);
+		$date_inscription = new DateTime();
+		$requete->execute(array(
+							'username' => $username,
+							'password' => $pwd,
+							'email' => $email,
+							'date_inscription' => $date_inscription->format('Y-m-d H:i:s')
+							  ));
+	}
+
 //compteur
 	$file_handler = fopen('compteur.txt', 'r+');
 	$compteur = fgets($file_handler);
--- www/inscription.php
+++ www/inscription.php
@@ -1,57 +1,242 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>WebAnnonces</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-<div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Web Annonces</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Accueil</a></li>
-            <li><a href="a-propos.php">A propos</a></li>
-            <li><a href="contact.php">Contact</a></li>
-          </ul>
-          <form class="navbar-form navbar-right" method="post" action="connexion.php">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Mot de passe" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Connexion</button>
-          </form>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </div>
+<?php
+	
+    include('inc/header.php');
+?>
 
 
     <div class="container" style="margin-top:100px;">
       <div class="row">
         <div class="col-lg-9">
-          <h1>Inscription</h1>
+			<?php
+				$db = connect_webannonces_db();
+			?>
+			<h2>Inscription</h2>
+			
+<?php
+			
+			
+    $errors = array();
+	 $form = array();
+	if(isset($_POST['submit_signin']))
+	{
+		$form['username'] = addslashes($_POST['form_username']);
+		$form['email'] = addslashes($_POST['form_email']);
+		$form['pwd1'] = $_POST['form_pwd1'];
+		$form['pwd2'] = $_POST['form_pwd2'];
+		
+		echo 'username :'.$form['username'].'<br>';
+		echo 'email :'.$form['email'].'<br>';
+		$is_username_exists = is_username_already_exists($db, $form['username']);
+		$is_email_exists = is_email_already_exists($db, $form['email']);
+		
+		//name field
+		if(strlen($form['username']) == 0)
+		{
+			$errors[] = array('message' => 'error on username field - mandatory field', 'field' => 'username');
+		}
+		elseif(strlen($form['username']) > 40)
+		{
+			$errors[] = array('message' => 'error on username field - max character : 40', 'field' => 'username');
+		}
+		
+		elseif ($is_username_exists)
+		{
+			$errors[] = array('message' => 'error on username field - username already exists', 'field' => 'username');
+		}
+		
+		//email field
+		
+		if(strlen($form['email']) == 0)
+		{
+			$errors[] = array('message' => 'error on email field - mandatory field', 'field' => 'email');
+		}
+		elseif(strlen($form['email']) > 100)
+		{
+			$errors[] = array('message' => 'error on email field - max character : 100' , 'field' => 'email');
+		}
+		elseif(strpos($form['email'], '@') === false) // [false = @ not found ]or [@ in first position strpos return 0]
+		{
+			$errors[] = array('message' => 'error on email field - @ missing' , 'field' => 'email');
+		}
+		
+		elseif(strpos($form['email'], '.') === false) // [false = . not found ]or [. in first position strpos return 0]
+		{
+			$errors[] = array('message' => 'error field - . missing' , 'field' => 'email');
+		}
+		elseif ($is_email_exists)
+		{
+			$errors[] = array('message' => 'error on email field - email already exists', 'field' => 'email');
+		}
+		
+		// password fields
+		if(strlen($form['pwd1']) < 6)
+		{
+			$errors[] = array('message' => 'error on password field - mandatory field', 'field' => 'pwd1');
+		}
+		if(strlen($form['pwd1']) > 40)
+		{
+			$errors[] = array('message' => 'error on password field - mandatory field', 'field' => 'pwd1');
+		}
+		if($form['pwd1'] != $form['pwd2'])
+		{
+			$errors[] = array('message' => 'error on password field - passwords do not match', 'field' => 'pwd1');
+		}
+		
+		// error checkbox
+	}
+if ((count($errors) == 0) && (isset($_POST['submit_signin'])))
+{
+	$pwd = md5($form['pwd1']);
+	add_newuser($db, $form['username'], $pwd, $form['email']);
+	 //add_newuser($db, $form['name'], $user,  $form['description']);
+}
+	
+?>	
 		  
-		  <p>Merci de vous inscrire à Web Annonces</p>
-		  
-		  
-        </div>
+			<p>Thanks to subscribe to Trello Like</p>
+<?php
+if (count($errors) > 0)
+{
+?>
+			<div class="alert alert-warning">
+				<strong>errors : </strong>
+				<ul>
+			
+			<?php
+			foreach($errors as $error)
+			{
+			?>
+				<li><?php echo $error['message']; ?> </li>
+			<?php
+			}
+			?>
+				</ul>
+			</div>
+<?php
+}
+?> 
+			<form method="post" action=""  class="form-horizontal" role="form">
+  
+				<div class="form-group
+					<?php 
+						if(count(error_by_field($errors, 'username')) > 0)
+						{
+							echo' has-error';
+						}
+						elseif((count(error_by_field($errors, 'username')) == 0) && (isset($_POST['submit_signin']))) 
+						{
+							echo' has-success';
+						}
+				
+			?>">
+					<label for="inputUsername" class="col-lg-3 control-label">Username</label>
+					<div class="col-lg-9">
+						<input type="text" name="form_username" class="form-control" id="inputUsername" placeholder="Enter Username" value="<?php 
+				if(isset($_POST['reset_signin']))
+				{
+					echo "";
+				}
+				else 
+				{
+					echo save_origin_value_from_form($form, 'username'); 
+				}
+				?>" />
+					</div>
+				</div>
+				<div class="form-group
+				<?php 
+						if(count(error_by_field($errors, 'email')) > 0)
+						{
+							echo' has-warning';
+						}
+						elseif((count(error_by_field($errors, 'email')) == 0) && (isset($_POST['submit_signin']))) 
+						{
+							echo' has-success';
+						}
+				
+			?>">
+					<label for="inputEmail1" class="col-lg-3 control-label">Email</label>
+					<div class="col-lg-9">
+						<input type="email" name="form_email" class="form-control" id="inputEmail" placeholder="Email" value="<?php 
+				if(isset($_POST['reset_signin']))
+				{
+					echo "";
+				}
+				else 
+				{
+					echo save_origin_value_from_form($form, 'email'); 
+				}
+				?>" />
+					</div>
+				</div>
+				<div class="form-group
+				<?php 
+						if(count(error_by_field($errors, 'pwd1')) > 0)
+						{
+							echo' has-error';
+						}
+						elseif((count(error_by_field($errors, 'pwd1')) == 0) && (isset($_POST['submit_signin']))) 
+						{
+							echo' has-success';
+						}
+				
+			?>">
+					<label for="inputPassword1" class="col-lg-3 control-label">Password</label>
+					<div class="col-lg-9">
+						<input type="password" name="form_pwd1" class="form-control" id="inputPassword1" placeholder="Password" value="<?php 
+				if(isset($_POST['reset_signin']))
+				{
+					echo "";
+				}
+				else 
+				{
+					echo save_origin_value_from_form($form, 'pwd1'); 
+				}
+				?>" />
+					</div>
+				</div>
+				<div class="form-group
+				<?php 
+						if(count(error_by_field($errors, 'pwd1')) > 0)
+						{
+							echo' has-error';
+						}
+						elseif((count(error_by_field($errors, 'pwd1')) == 0) && (isset($_POST['submit_signin']))) 
+						{
+							echo' has-success';
+						}
+				
+			?>">
+					<label for="inputPassword2" class="col-lg-3 control-label">Confirm Password</label>
+					<div class="col-lg-9">
+						<input type="password" name="form_pwd2" class="form-control" id="inputPassword2" placeholder="Password" value="<?php 
+				if(isset($_POST['reset_signin']))
+				{
+					echo "";
+				}
+				else 
+				{
+					echo save_origin_value_from_form($form, 'pwd2'); 
+				}
+				?>" />
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-lg-offset-3 col-lg-9">
+						<div class="checkbox">
+							<label><input type="checkbox"> Accept General Conditions
+							</label>
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-lg-offset-2 col-lg-10">
+						<input id="submit_signin" name="submit_signin" type="submit" class="btn btn-success" value="Sign in" />
+						<input id="reset_signin" name="reset_signin" type="submit" class="btn btn-default" value="Cancel" />
+					</div>
+				</div>
+			</form>
+		</div>
         <div class="col-lg-3 well">
 			<h3>Dernières annonces</h3>
 
